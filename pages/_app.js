4df167ba4110f2a9a15b29dae7e734fa2ad3feec
aimeliala <idstack@outlook.co.id>
2023-11-05 22:20:39 +0700

    setup login and logout state

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import "@/styles/globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <UserProvider>
       <Head>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </UserProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { isLoading, error, user } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
   return (
     <div>
       <Head>
         <title>Next JS ChatGPT Project</title>
       </Head>
-      <h1>Welcome to the Next JS ChatGPT Project</h1>
+      <h1>Welcome to the Next JS ChatGPT Project X</h1>
+      <div>
+        {!!user && <Link href="api/auth/logout">Logout</Link>}
+        {!user && <Link href="api/auth/login">Login</Link>}
+      </div>
     </div>
   );
 }
