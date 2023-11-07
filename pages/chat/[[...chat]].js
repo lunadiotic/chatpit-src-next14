1d2ba46bc8cc6e60f87e0a88050ddac9e6225c63
aim <ahmadhakim404@gmail.com>
2023-11-07 23:06:55 +0700

    signup and redirect to chat if logged in

---
+++ pages/chat/[[...chat]].js
@@ -0,0 +1,9 @@
+
+export default function Chat() {
+
+    return (
+        <>
+            <h1>Chat</h1>
+        </>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default function Home() {
   const { isLoading, error, user } = useUser();
@@ -26,4 +27,20 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+export async function getServerSideProps({ req, res }) {
+  const session = await getSession(req, res);
+  if (!!session) {
+    return {
+      redirect: {
+        destination: '/chat',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
