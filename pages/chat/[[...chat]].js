850c8b1aad07469f36e81af6091289c3ee663033
aim <ahmadhakim404@gmail.com>
2023-11-07 23:17:36 +0700

    make layout chat page

--- pages/chat/[[...chat]].js
+++ pages/chat/[[...chat]].js
@@ -1,9 +1,20 @@
+import Head from "next/head";
 
 export default function Chat() {
-
     return (
         <>
-            <h1>Chat</h1>
+            <Head>
+                <title>Start Chat</title>
+            </Head>
+            <div className="grid h-screen grid-cols-[260px_1fr]">
+                <div className="bg-slate-700">
+                    sidebar
+                </div>
+                <div className="flex flex-col bg-slate-800">
+                    <div className="flex-1">chat window</div>
+                    <footer className="p-4 bg-slate-900">Footer</footer>
+                </div>
+            </div>
         </>
     );
 }
