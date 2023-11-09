3cc9cc83960024aa405e2cf8831d135d32a60849
aimeliala <idstack@outlook.co.id>
2023-11-09 15:45:09 +0700

    create function request to openai api

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -1,3 +1,5 @@
+import { OpenAIEdgeStream } from "openai-edge-stream";
+
 export const config = {
   runtime: "edge",
 };
@@ -5,6 +7,22 @@ export const config = {
 export default async function handler(req) {
   try {
     const { message } = await req.json();
+    const stream = await OpenAIEdgeStream(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: message }],
+          stream: true,
+        }),
+      }
+    );
+    return new Response(stream);
   } catch (error) {
     console.log(error);
   }
